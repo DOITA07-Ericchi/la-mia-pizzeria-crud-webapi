c58e7985059ac7f950b3cf49c9ad142ab610e433
EricchiYukia <git@ericchi.moe>
2023-01-09 16:13:18 +0100

    Fatte cose

---
+++ La Mia Pizzeria 1/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace La_Mia_Pizzeria_1.Models {
+	public class Pizza {
+
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Image { get; set; }
+
+		public Pizza (int id, string title, string description, string image) {
+			Id = id;
+			Title = title;
+			Description = description;
+			Image = image;
+		}
+	}
+}
+
---
+++ La Mia Pizzeria 1/Utils/PizzaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace La_Mia_Pizzeria_1.Utils
+{
+	public static class PizzaData
+	{
+		private static List<Pizza> pizze;
+
+        public static List<Pizza> GetPosts()
+        {
+
+            if(pizze != null)
+            {
+                return pizze;
+			}
+
+            posts = new List<Post>();
+
+            for(int i = 0; i<10; i++)
+			{
+				Post postGenerato = new Post(i, "Titolo post: " + i, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://picsum.photos/id/" + i + "/300");
+				posts.Add(postGenerato);
+			}
+			return posts;
+		}
+	}
+}
+
