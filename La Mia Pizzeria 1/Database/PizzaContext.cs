58d28b30349c5353e7ddb0e1da91303ac7726c69
EricchiYukia <git@ericchi.moe>
2023-01-10 15:47:28 +0100

    Aggiunto il DB

---
+++ La Mia Pizzeria 1/Database/PizzaContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using La_Mia_Pizzeria_1.Models;
+
+namespace La_Mia_Pizzeria_1.Database {
+	public class PizzaContext : DbContext {
+		public DbSet<Pizza> pizzas { get; set; }
+
+		protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer ("Data Source=localhost;Database=Pizzabase;User Id=SA;Password=nekonekonyaa9021;TrustServerCertificate=True");
+		}
+	}
+}
--- La Mia Pizzeria 1/Models/Pizza.cs
+++ La Mia Pizzeria 1/Models/Pizza.cs
@@ -5,14 +5,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace La_Mia_Pizzeria_1.Models {
 	public class Pizza {
 		public int Id { get; set; }
-		[Column(TypeName = "varchar(128)")]
+		[Column (TypeName = "varchar(128)")]
 		public string Title { get; set; }
-		[Column(TypeName = "text")]
+		[Column (TypeName = "text")]
 		public string Description { get; set; }
-		[Column(TypeName = "decimal(5,2)")]
+		[Column (TypeName = "decimal(5,2)")]
 		public double Price { get; set; }
-		[Column(TypeName = "varchar(512)")]
+		[Column (TypeName = "varchar(512)")]
 		public string Image { get; set; }
+
+		public Pizza () {
+
+		}
 
 		public Pizza (int id, string title, string description, double price, string image) {
 			Id = id;
