c71d6f542fa701ad7494af9b0efbc05f7f215598
EricchiYukia <git@ericchi.moe>
2023-01-10 15:29:08 +0100

    EF aggiunto

--- La Mia Pizzeria 1/Models/Pizza.cs
+++ La Mia Pizzeria 1/Models/Pizza.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace La_Mia_Pizzeria_1.Models {
 	public class Pizza {
 		public int Id { get; set; }
+		[Column(TypeName = "varchar(128)")]
 		public string Title { get; set; }
+		[Column(TypeName = "text")]
 		public string Description { get; set; }
+		[Column(TypeName = "decimal(5,2)")]
 		public double Price { get; set; }
+		[Column(TypeName = "varchar(512)")]
 		public string Image { get; set; }
 
 		public Pizza (int id, string title, string description, double price, string image) {
