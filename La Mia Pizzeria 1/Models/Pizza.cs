4ba079770b9db20ebebfa9ed3d491e378feaa1ed
EricchiYukia <git@ericchi.moe>
2023-01-12 16:03:33 +0100

    Model modificato e fatta pagina update

--- La Mia Pizzeria 1/Models/Pizza.cs
+++ La Mia Pizzeria 1/Models/Pizza.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace La_Mia_Pizzeria_1.Models {
 	public class Pizza {
 		public int Id { get; set; }
 		[Column (TypeName = "varchar(128)")]
+		[StringLength(50, ErrorMessage="Il campo titolo non può contenere più di 50 caratteri")]
 		public string Title { get; set; }
 		[Column (TypeName = "text")]
 		public string Description { get; set; }
 		[Column (TypeName = "decimal(5,2)")]
 		public double Price { get; set; }
+		[StringLength(512, ErrorMessage="Il campo immagine non può contenere più di 512 caratteri")]
 		[Column (TypeName = "varchar(512)")]
 		public string Image { get; set; }
 
