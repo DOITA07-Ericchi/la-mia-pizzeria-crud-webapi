c6237db5cff041fba3753b8abbfd3d0fa3ea723b
Ericchi <git@ericchi.moe>
2023-01-23 16:34:51 +0100

    Api lettura completata

---
+++ La Mia Pizzeria 1/Controllers/Api/PizzasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using La_Mia_Pizzeria_1.Database;
+using La_Mia_Pizzeria_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace La_Mia_Pizzeria_1.Controllers.Api {
+	[Route ("api/[controller]")]
+	[ApiController]
+	public class PizzasController : Controller {
+		// GET: api/values
+		[HttpGet]
+		public IActionResult Get () {
+			using (PizzaContext db = new PizzaContext()) {
+				List<Pizza> articoli = db.pizzas
+					// .Include(pizzona => pizzona.Tags) // Non ho fatto i tag...
+					.ToList<Pizza>();
+				return Ok(articoli);
+			}
+		}
+	}
+}
+
