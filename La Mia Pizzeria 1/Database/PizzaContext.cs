fe985c6294a3bb4875ea8daf8eaa976903ae3c30
Ericchi <git@ericchi.moe>
2023-01-25 15:56:25 +0100

    Pre-codegenerator

--- La Mia Pizzeria 1/Database/PizzaContext.cs
+++ La Mia Pizzeria 1/Database/PizzaContext.cs
@@ -2,9 +2,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using La_Mia_Pizzeria_1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace La_Mia_Pizzeria_1.Database {
-	public class PizzaContext : DbContext {
+	public class PizzaContext : IdentityDbContext {
 		public DbSet<Pizza> pizzas { get; set; }
 		public DbSet<Variant> variants {get; set; }
 
