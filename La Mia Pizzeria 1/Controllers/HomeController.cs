a2df96567001548efcdbcc88d9c90a2dfb50f02c
Ericchi <git@ericchi.moe>
2023-01-25 15:51:14 +0100

    Iniziato il login

--- La Mia Pizzeria 1/Controllers/HomeController.cs
+++ La Mia Pizzeria 1/Controllers/HomeController.cs
@@ -4,29 +4,27 @@ using La_Mia_Pizzeria_1.Models;
 
 namespace La_Mia_Pizzeria_1.Controllers;
 
-public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
+public class HomeController : Controller {
+	private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+	public HomeController (ILogger<HomeController> logger) {
+		_logger = logger;
+	}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+	public IActionResult Index () {
+		return View ();
+	}
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+	public IActionResult Privacy () {
+		return View ();
+	}
+	public IActionResult Details () {
+		return View ();
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	[ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error () {
+		return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
 
